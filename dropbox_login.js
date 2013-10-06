ec59880a2c5e6dae075074bbfe34473d2c19e3ea
Brian Le <blsymph@gmail.com>
2013-10-06 01:27:04 +0000

    Add emitter for returning data from phantomjs's evaluate()

--- dropbox_login.js
+++ dropbox_login.js
@@ -1,6 +1,7 @@
-function dropboxAuthenticate () {
+function DropboxAuthentication () {
     var dbox = require('dbox'),
-        phantom = require('phantom');
+        phantom = require('phantom'),
+        eventEmitter = require('eventemitter2').EventEmitter2;
 
     var APP_KEY = 'ht7wkxiaq9t3lm8',
         APP_SECRET = '0pwb5sy2aveu7sc';
@@ -8,40 +9,51 @@ function dropboxAuthenticate () {
     var app = dbox.app({
         'app_key': APP_KEY,
         'app_secret': APP_SECRET
-    });
+    }),
+        emitter = new eventEmitter();
 
-    app.requesttoken(function (status, requestToken) {
-        console.log(requestToken);
-        phantom.create(function (ph) {
-            ph.createPage(function (page) {
-                page.open(requestToken.authorize_url, function (status) {
-                    console.log(status);
-                    page.includeJs('http://code.jquery.com/jquery-1.10.1.min.js', function () {
-                        page.evaluate((function () {
-                            // document.getElementById('login_email').value = 'notchdastheny@maildrop.cc';
-                            document.getElementById('login_email').value = 'temp6969@yahoo.com';
-                            document.getElementById('login_password').value = 'temp69691';
-                            // document.getElementsByName('login_submit_dummy')[0].click();
-                            document.getElementById('login_submit').click();
-                        }), function (result) {
-                            setTimeout(function() {
-                                page.render('dropbox.png');
-                                ph.exit();
-                            }, 2000);
+    function login (email, password, callback) {
+        app.requesttoken(function (status, requestToken) {
+            // add event listener
+            emitter.on('render', callback(requestToken));
+
+            console.log(requestToken);
+            console.log(status);
+            phantom.create(function (ph) {
+                ph.createPage(function (page) {
+                    page.open(requestToken.authorize_url, function (status) {
+                        page.includeJs('http://code.jquery.com/jquery-1.10.1.min.js', function () {
+                            page.includeJs('//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js', function () {
+                                page.evaluate((function (email, password, emitter, requestToken, callback) {
+                                    document.getElementById('login_email').value = email;
+                                    document.getElementById('login_password').value = password;
+                                    document.getElementById('login_submit').click();
+                                }), function (result) {
+                                    emitter.emit('render', requestToken);
+                                    setTimeout(function () {
+                                        ph.exit();
+                                    }, 1000);
+                                }, email, password, emitter, requestToken, callback);
+                            });
                         });
                     });
                 });
             });
         });
-    });
-
-    function login () {
     }
 
     function logout () {
     }
+
+    function run (callback) {
+        var email = 'temp6969@yahoo.com',
+            password = 'temp69691';
+
+        login(email, password, callback);
+
+    }
+
+    return run;
 }
 
-// module.exports = dropboxAuthenticate;
-
-dropboxAuthenticate();
+module.exports = DropboxAuthentication;
